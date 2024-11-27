a1c0d1629d411b9f91daec6553c552b73b67410e
parsazehtab <parsaz00@students.cs.ubc.ca>
2024-11-26 21:49:09 -0800

    Update

--- setup_mac.sh
+++ setup_mac.sh
@@ -1,3 +1,5 @@
+#run chmod +x ./setup_mac.sh to allow it to run 
+
 #!/bin/bash
 brew install gtk+3 haskell-stack
 echo 'export PKG_CONFIG_PATH=$(brew --prefix gtk+3)/lib/pkgconfig:$PKG_CONFIG_PATH' >> ~/.zshrc
