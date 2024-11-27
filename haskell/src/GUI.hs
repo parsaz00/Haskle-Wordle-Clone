13be6073a8f69edf9b6351046268e8ba789eb115
Daichi Furukawa <daichifg0626@gmail.com>
2024-11-26 21:28:51 -0800

    fixed the gui to show 7 rows

--- haskell/src/GUI.hs
+++ haskell/src/GUI.hs
@@ -106,11 +106,11 @@ launchGUI = do
   containerAdd window vbox
 
   -- Create a grid for displaying guesses
-  grid <- tableNew 6 5 True
+  grid <- tableNew 6 7 True
   boxPackStart vbox grid PackGrow 0
 
   -- Populate the grid with empty labels
-  labels <- mapM (mapM (\_ -> labelNew (Just "_"))) (replicate 6 (replicate 5 ()))
+  labels <- mapM (mapM (\_ -> labelNew (Just "_"))) (replicate 6 (replicate 7 ()))
   zipWithM_ (\row rowWidgets -> zipWithM_ (\col widget -> tableAttachDefaults grid widget col (col+1) row (row+1)) [0..] rowWidgets) [0..] labels
 
   -- Add a text entry for guesses
